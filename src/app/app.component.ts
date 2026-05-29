0693a4047d71e454c9a56ae0efd897552401ce5a
Jorge T <jotecpavi4939@uaemex.mx>
2026-05-28 18:55:58 -0600

    Integración del componente comp-inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
+import { CompInlineComponent } from "./comp-inline/comp-inline.component";
 
 @Component({
   selector: 'app-root', //nombre de la plantilla que vamos a usar
-  imports: [RouterOutlet, NuevoComponenteComponent], //Permite el cambio de rutas dentro de la palicación
+  imports: [RouterOutlet, NuevoComponenteComponent, CompInlineComponent], //Permite el cambio de rutas dentro de la palicación
   templateUrl: './app.component.html', //Define el codigo HTML
   styleUrl: './app.component.css'
 })
