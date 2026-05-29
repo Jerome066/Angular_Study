9748cef43f90f24ebe08873dca3cc354432a1a29
Jorge T <jotecpavi4939@uaemex.mx>
2026-05-29 10:03:22 -0600

    incorporación del componente padre en APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 import { CompInlineComponent } from "./comp-inline/comp-inline.component";
 import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
+import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root', //nombre de la plantilla que vamos a usar
-  imports: [RouterOutlet, NuevoComponenteComponent, CompInlineComponent, InterpolacionComponent], //Permite el cambio de rutas dentro de la palicación
+  imports: [RouterOutlet, NuevoComponenteComponent, CompInlineComponent, InterpolacionComponent, PadreComponent], //RouterOutlet Permite el cambio de rutas dentro de la palicación
   templateUrl: './app.component.html', //Define el codigo HTML
   styleUrl: './app.component.css'
 })
