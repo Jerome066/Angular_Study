57326cc8c796c0c667fa3f96e8a9149e0509759a
Jorge T <jotecpavi4939@uaemex.mx>
2026-05-28 18:22:44 -0600

    Agregar nuevo componente al APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 
 @Component({
   selector: 'app-root', //nombre de la plantilla que vamos a usar
-  imports: [RouterOutlet], //Permite el cambio de rutas dentro de la palicación
+  imports: [RouterOutlet, NuevoComponenteComponent], //Permite el cambio de rutas dentro de la palicación
   templateUrl: './app.component.html', //Define el codigo HTML
   styleUrl: './app.component.css'
 })
